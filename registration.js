71700ff134905422a7910dd205de04bf012e125c
Suahco4 <Thesuah22@gmail.com>
2025-09-06 19:34:08 +0000

    Update registration.js

--- registration.js
+++ registration.js
@@ -1,17 +1,19 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyDBw0ZIzZtsUJr5PnRZiv6VP27h3fUxAxM",
-  authDomain: "suahco4.firebaseapp.com",
-  projectId: "suahco4",
-  storageBucket: "suahco4.firebasestorage.app",
-  messagingSenderId: "953429512465",
-  appId: "1:953429512465:web:8f20dc4972d94be7f87cb0"
-};
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDBw0ZIzZtsUJr5PnRZiv6VP27h3fUxAxM",
+    authDomain: "suahco4.firebaseapp.com",
+    projectId: "suahco4",
+    storageBucket: "suahco4.firebasestorage.app",
+    messagingSenderId: "953429512465",
+    appId: "1:953429512465:web:21fbaac17865c653f87cb0"
+  };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
