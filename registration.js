af38dfc5170356fb4d80d9c363990459e372028e
Suahco4 <Thesuah22@gmail.com>
2025-09-06 19:34:47 +0000

    Update registration.js

--- registration.js
+++ registration.js
@@ -1,5 +1,5 @@
-<script type="module">
-  // Import the functions you need from the SDKs you need
+
+// Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
@@ -16,4 +16,3 @@
 
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
-</script>
