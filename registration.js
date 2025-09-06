1ce88b2b7fb40072104639754f67c1b9e7cee4e8
Suahco4 <Thesuah22@gmail.com>
2025-09-06 19:28:13 +0000

    Create registration.js

---
+++ registration.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDBw0ZIzZtsUJr5PnRZiv6VP27h3fUxAxM",
+  authDomain: "suahco4.firebaseapp.com",
+  projectId: "suahco4",
+  storageBucket: "suahco4.firebasestorage.app",
+  messagingSenderId: "953429512465",
+  appId: "1:953429512465:web:8f20dc4972d94be7f87cb0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
